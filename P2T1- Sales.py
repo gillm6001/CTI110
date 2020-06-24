7fe6c72220805e4e1d83f386e875ad652c19c024
gillm6001 <43529057+gillm6001@users.noreply.github.com>
2020-06-23 20:54:16 -0700

    Add files via upload

---
+++ P2T1- Sales.py
@@ -0,0 +1,22 @@
+#A Basic math coding script
+#6-23-2020
+#CTI-110 P2HW1-Basic Math
+#Marcell Gill
+#
+
+# Addition
+x = input("Enter the first number: ")
+y = input("Enter the second number: ")
+total = str(x + y)
+print("The addition of those 2 numbers is " + total)
+print()
+
+# Multiplication 
+x = int(input("Enter the first number: "))
+y  = int(input("Enter the second number: "))
+total = str(x * y)
+print("The multiplication of those 2 numbers is " + total)
+print()
+    
+
+
